974cad956595c0113a7e2eddab69b7190a440ef0
Luxx <lkgames256@gmail.com>
2024-03-09 12:04:53 +0100

    updates

--- update.sh
+++ update.sh
@@ -4,5 +4,9 @@ sudo rm -r "aurora"
 
 git clone https://github.com/LuxxBlockyy-Alliance/aurora.git
 
+echo ""
+echo "--------------------- Starting Aurora ---------------------"
+echo ""
+
 python3 aurora/run.py
 
