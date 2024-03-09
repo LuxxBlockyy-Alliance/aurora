f32b7018814c74dd98e1f7e577d91245f72c8797
blockplacer4 <blockplacer4@gmail.com>
2024-03-09 16:32:26 +0100

    1.0.3

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -22,7 +22,7 @@ async def get_chatgpt_response(messages):
     msg.append(messages)
     response = await client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=messages
+        messages=msg
     )
     return response.choices[0].message.content
 
